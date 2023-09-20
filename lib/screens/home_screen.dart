a5a0135b27d22980699426a45f39dd89619e8620
CharlesHauer <106684183+CharlesHauer@users.noreply.github.com>
2023-09-20 17:34:33 +0200

    add feature: connect nav bar with screen

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,5 +1,8 @@
+import 'package:beer_connect/screens/help_screen.dart';
 import 'package:beer_connect/screens/scan_screen.dart';
 import 'package:flutter/material.dart';
+
+import 'history_screen.dart';
 
 class HomeScreen extends StatefulWidget {
   const HomeScreen({super.key});
@@ -9,15 +12,39 @@ class HomeScreen extends StatefulWidget {
 }
 
 class _HomeScreenState extends State<HomeScreen> {
+  int _currentIndex = 1;
+
+  setCurrentIndex(int index) {
+    setState(() {
+      _currentIndex = index;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: const ScanScreen(),
+      body: [
+        const HistoryScreen(),
+        const ScanScreen(),
+        const HelpScreen(),
+      ][_currentIndex],
+
       bottomNavigationBar: BottomNavigationBar(
+        currentIndex: _currentIndex,
+        onTap: (index) =>  setCurrentIndex(index),
         items: const [
-          BottomNavigationBarItem(icon: Icon(Icons.history), label: 'historique'),
-          BottomNavigationBarItem(icon: Icon(Icons.document_scanner_outlined), label: 'scan'),
-          BottomNavigationBarItem(icon: Icon(Icons.help), label: 'aide'),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.history),
+            label: 'historique',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.document_scanner_outlined),
+            label: 'scan'
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.help),
+            label: 'aide',
+          ),
         ],
       ),
     );
